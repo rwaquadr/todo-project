e3e31088077402069019e2a05919c5f4e8a76c06
Robb Aquadro <rwaquadr@gmail.com>
2017-07-05 13:32:41 -0500

    Next commit

--- server.js
+++ server.js
@@ -12,8 +12,47 @@ app.listen(port, function(err) {
 
 
 
-app.get('/test-app', function(request, response) {  
+app.get('/todo-app', function(request, response) {  
   response.send('Hello from Express!');
   console.log('route succesfully getting hit');
 });
 
+
+var allTodos = [{
+	"description" : "feed the dog",
+	"due date" : "7/5/17",
+	"ID" : 1,
+},
+
+{
+	"description" : "wash the dishes",
+	"due date" : "7/6/17",
+	"ID" : 2,
+},
+
+{
+	"description" : "take out the trash",
+	"due date" : "7/7/17",
+	"ID" : 3,
+},]
+
+
+
+app.get('/get-allTodos', function(request, response){
+	response.send(allTodos);
+	console.log('this route is being hit');
+});
+
+app.post('/post-allTodos', function(request, response){
+	request.body(allTodos);
+	console.log('this works')
+	//console log request.body (make sure you're gettign the args from postman)
+	//saving the object to your array of objects
+	//
+ 
+
+
+
+	//response.send(request.body);
+	// console.log('this route is being hit');
+});
